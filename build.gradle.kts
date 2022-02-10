f3d75af9a72e349cd16f0e63efeea2f4ddf3ea27
Milad Nekofar <milad@nekofar.com>
2022-02-10 08:04:34 +0330

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.jetbrains.intellij") version "1.3.1"
+    kotlin("jvm") version "1.6.20-M1"
+    java
+}
+
+group = "com.nekofar.milad"
+version = "1.0.0-alpha.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2021.3.2")
+}
+tasks {
+    patchPluginXml {
+        changeNotes.set("""
+            Add change notes here.<br>
+            <em>most HTML tags may be used</em>        """.trimIndent())
+    }
+}
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
