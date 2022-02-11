e5c9ba9636e3764638384fd6a9c5a1d27dd5a83c
Milad Nekofar <milad@nekofar.com>
2022-02-11 14:08:20 +0330

    build: retrieve plugin description from readme file instead of `plugin.xml`

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,19 @@ tasks {
         sinceBuild.set(properties("pluginSinceBuild"))
         untilBuild.set(properties("pluginUntilBuild"))
 
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            projectDir.resolve("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").run { org.jetbrains.changelog.markdownToHTML(this) }
+        )
+
         // Get the latest available change notes from the changelog file
         changeNotes.set(provider {
             changelog.run {
