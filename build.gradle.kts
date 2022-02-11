69845d2686404c58bece0e89288d26c2fba3bb75
Milad Nekofar <milad@nekofar.com>
2022-02-11 13:41:09 +0330

    build: add qodana gradle plugin to analysis code quality

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
+    // Java support
+    id("java")
+    // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "1.6.20-M1"
+    // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij") version "1.3.1"
-    kotlin("jvm") version "1.6.20-M1"
-    java
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
+    // Gradle Qodana Plugin
+    id("org.jetbrains.qodana") version "0.1.13"
 }
 
 group = "com.nekofar.milad"
@@ -38,6 +43,14 @@ changelog {
     keepUnreleasedSection.set(true)
     unreleasedTerm.set("[unreleased]")
     groups.set(emptyList())
+}
+
+// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
+qodana {
+    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
+    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
+    saveReport.set(true)
+    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
 }
 
 tasks {
