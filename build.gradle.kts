ea444e99bfc6f6714aadf49c0af4f4bed9c98416
Milad Nekofar <milad@nekofar.com>
2023-08-14 11:08:52 +0330

    build: disable searchable options in Gradle build script
    Added a buildSearchableOptions block in the 'build.gradle.kts' file and set enabled to false. This was done to potentially improve build performance by preventing Gradle from indexing certain options.

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,10 @@ tasks {
         gradleVersion = properties("gradleVersion").get()
     }
 
+    buildSearchableOptions {
+        enabled = false
+    }
+
     patchPluginXml {
         version = properties("pluginVersion")
         sinceBuild = properties("pluginSinceBuild")
